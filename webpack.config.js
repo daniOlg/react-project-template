603b44c2a0d27215ad9de4d6c096024c4cd8f4e8
Daniel Olguin <dani.olg@hotmail.com>
2022-08-21 23:06:31 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public/assets', to: 'assets' }],
+    }),
+  ],
+};
